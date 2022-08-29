bae9cebf56796e90e58ad1566320ac6e79236efa
Natalia Panfilenka <choco-cat@mail.ru>
2022-08-29 21:02:39 +0300

    init: create app structure

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <link href="./assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+<h1>Игра крестики-нолики
+</h1>
+<table align="center" class="game-table">
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+</body>
+</html>
