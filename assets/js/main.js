328e8ca84e4833b6976c3d27336efa3c669ca674
Natalia Panfilenka <choco-cat@mail.u>
2022-08-30 11:22:27 +0300

    feat: put user cross

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 $(document).ready(function () {
+    getBoard();
     $('.game-table td').click(
         function () {
+            $(this).html('X');
             sendData({row: $(this).parent().index(), col: $(this).index()}, './helpers/game.php');
             return false;
         }
     );
 });
+
+function getBoard() {
+    const board = [[], [], []];
+    $('.game-table td').each(function () {
+        board[$(this).parent().index()][$(this).index()] = $(this).text();
+    });
+    console.log(board);
+};
 
 function sendData(data, url) {
     $.ajax({
