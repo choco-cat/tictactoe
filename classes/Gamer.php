7a6bde77743f20ae3efa2a4c7d263ea4431b90d2
Natalia Panfilenka <choco-cat@mail.u>
2022-08-30 15:02:13 +0300

    refactor: create Gamer class

---
+++ classes/Gamer.php
@@ -0,0 +1,106 @@
+<?php
+
+class Gamer
+{
+    const BOARD_SIZE = 3;
+    public $board = array();
+
+    public function __construct($board)
+    {
+        $this->board = $board;
+    }
+
+    public function nextStep()
+    {
+        foreach ($this->board as $indexRow => $row) {
+            foreach ($row as $indexColumn => $cell) {
+                if ($cell === '') {
+                    if ($this->checkLine($this->board, SYMBOL_BOT, $indexRow, $indexColumn)) {
+                        return array(
+                            'row' => $indexRow,
+                            'col' => $indexColumn
+                        );
+                    }
+                }
+            }
+        }
+
+        if ($this->board[1][1] === '') {
+            return array(
+                'row' => 1,
+                'col' => 1
+            );
+        }
+        for ($rowIndex = 0; $rowIndex < self::BOARD_SIZE - 1; $rowIndex += 2) {
+            for ($colIndex = 0; $colIndex < self::BOARD_SIZE - 1; $colIndex += 2) {
+                if ($this->board[$rowIndex][$colIndex] === '') {
+                    return array(
+                        'row' => $rowIndex,
+                        'col' => $colIndex
+                    );
+                    break;
+                }
+            }
+        }
+        foreach ($this->board as $indexRow => $row) {
+            foreach ($row as $indexColumn => $cell) {
+                if ($cell === '') {
+                    return array(
+                        'row' => $indexRow,
+                        'col' => $indexColumn
+                    );
+                    break;
+                }
+            }
+        }
+        return array(
+            'row' =>'',
+            'col' => ''
+        );
+    }
+
+    public function checkGameOverUser()
+    {
+        $message = '';
+        if ($this->checkDiagonal('X') || $this->checkLine('X')) {
+            $message = 'Congratulations! You won the game!';
+        }
+        return $message;
+    }
+
+    public function checkGameOverBot($step)
+    {
+        $this->board[$step['row']][$step['col']] = '0';
+        $message = '';
+        if ($this->checkDiagonal('0') || $this->checkLine('0')) {
+            $message = 'You lost the game!';
+        }
+        return $message;
+    }
+
+
+    private function checkLine($symb)
+    {
+        for ($col = 0; $col < self::BOARD_SIZE; $col++) {
+            $cols = true;
+            $rows = true;
+            for ($row = 0; $row < self::BOARD_SIZE; $row++) {
+                $cols = $cols && ($this->board[$col][$row] === $symb);
+                $rows = $rows && ($this->board[$row][$col] === $symb);
+            }
+            if ($cols || $rows) return true;
+        }
+        return false;
+    }
+
+    private function checkDiagonal($symb)
+    {
+        $rightDiagonal = true;
+        $leftDiagonal = true;
+        for ($i = 0; $i < self::BOARD_SIZE; $i++) {
+            $rightDiagonal = $rightDiagonal && ($this->board[$i][$i] === $symb);
+            $leftDiagonal = $leftDiagonal && $this->board[self::BOARD_SIZE - $i - 1][$i] === $symb;
+        }
+        return $rightDiagonal || $leftDiagonal;
+    }
+}
--- helpers/game.php
+++ helpers/game.php
@@ -1,109 +1,22 @@
 <?php
+const SYMBOL_BOT = '0';
+require_once('./../classes/Gamer.php');
+
 $data = $_POST;
 if (!$data['board']) {
     return;
 }
-const SYMBOL_BOT = '0';
-const BOARD_SIZE = 3;
 $board = $data['board'];
-$message = checkGameOverUser($board);
+$gamer = new Gamer($board);
+
+$message = $gamer->checkGameOverUser();
 if ($message) {
     echo json_encode(array('message' => $message));
     exit();
 }
-$step = nextStep($board);
+$step = $gamer->nextStep();
 $step['symb'] = SYMBOL_BOT;
 
-$board[$step['row']][$step['col']] = '0';
-$message = checkGameOverBot($board);
+$message = $gamer->checkGameOverBot($step);
 
 echo json_encode(array('step' => $step, 'message' => $message));
-
-function checkGameOverUser($board)
-{
-    $message = '';
-    if (checkDiagonal($board, 'X') || checkLine($board, 'X')) {
-        $message = 'Congratulations! You won the game!';
-    }
-    return $message;
-}
-
-function checkGameOverBot($board)
-{
-    $message = '';
-    if (checkDiagonal($board, '0') || checkLine($board, '0')) {
-        $message = 'You lost the game!';
-    }
-    return $message;
-}
-
-function nextStep($board)
-{
-    foreach ($board as $indexRow => $row) {
-        foreach ($row as $indexColumn => $cell) {
-            if ($cell === '') {
-                if (checkLine($board, SYMBOL_BOT, $indexRow, $indexColumn)) {
-                    return array(
-                        'row' => $indexRow,
-                        'col' => $indexColumn
-                    );
-                }
-            }
-        }
-    }
-
-    if ($board[1][1] === '') {
-        return array(
-            'row' => 1,
-            'col' => 1
-        );
-    }
-    for ($rowIndex = 0; $rowIndex < BOARD_SIZE - 1; $rowIndex += 2) {
-        for ($colIndex = 0; $colIndex < BOARD_SIZE - 1; $colIndex += 2) {
-            if ($board[$rowIndex][$colIndex] === '') {
-                return array(
-                    'row' => $rowIndex,
-                    'col' => $colIndex
-                );
-                break;
-            }
-        }
-    }
-    foreach ($board as $indexRow => $row) {
-        foreach ($row as $indexColumn => $cell) {
-            if ($cell === '') {
-                return array(
-                    'row' => $indexRow,
-                    'col' => $indexColumn
-                );
-                break;
-            }
-        }
-    }
-    return array();
-}
-
-function checkLine($board, $symb)
-{
-    for ($row = 0; $row < BOARD_SIZE; $row++) {
-        $cols = true;
-        $rows = true;
-        for ($col = 0; $col < BOARD_SIZE; $col++) {
-            $cols = $cols && ($board[$row][$col] === $symb);
-            $rows = $rows && ($board[$col][$row] === $symb);
-        }
-        return $cols || $rows;
-    }
-    return false;
-}
-
-function checkDiagonal($board, $symb)
-{
-    $rightDiagonal = true;
-    $leftDiagonal = true;
-    for ($i = 0; $i < BOARD_SIZE; $i++) {
-        $rightDiagonal = $rightDiagonal && ($board[$i][$i] === $symb);
-        $leftDiagonal = $leftDiagonal && $board[BOARD_SIZE - $i - 1][$i] === $symb;
-    }
-    return $rightDiagonal || $leftDiagonal;
-}
